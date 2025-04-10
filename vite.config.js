813746d272c3e7bdc31b27efdeb05ef464f9a6c3
Calvorya M <calvorya@outlook.com>
2025-04-10 15:59:54 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/shadows-of-the-mirror/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
