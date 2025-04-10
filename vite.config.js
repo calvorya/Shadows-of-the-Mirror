35a09487ca9723f3101a3f5bcefd4c2bf610e46a
Calvorya M <calvorya@outlook.com>
2025-04-10 16:10:41 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,15 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom'],
         },
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
       },
     },
   },
