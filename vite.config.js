ad0ff47ec204f946bf19c29387cd46180a596255
Calvorya M <calvorya@outlook.com>
2025-04-10 16:29:47 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shadows-of-the-mirror/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  base: '/Shadows-of-the-Mirror/',
 })
