2dd6945914c28f1a7b5a0f957d1a64c73b7ed039
Calvorya M <calvorya@outlook.com>
2025-04-11 15:35:43 +0330

    UP 5

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Shadows-of-the-Mirror/',
+  base: '/shadows-of-the-mirror/',
   build: {
     outDir: 'dist',
   },
